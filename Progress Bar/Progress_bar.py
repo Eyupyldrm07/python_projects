61a2bd0bff31b1084c17553f90ea547e73d54b52
Eyüp Mustafa YILDIRIM <eyupyldrm07@gmail.com>
2024-07-13 22:23:04 +0300

    Python

--- Progress Bar/Progress_bar.py
+++ Progress Bar/Progress_bar.py
@@ -3,16 +3,16 @@ import progressbar
 import requests
 
 
-# def main():
-#     m = 100
-#     with progressbar.ProgressBar(max_value=m) as bar:
-#         for i in range(m):
-#             bar.update(i)
-#             time.sleep(0.2)
+def main():
+    m = 100
+    with progressbar.ProgressBar(max_value=m) as bar:
+        for i in range(m):
+            bar.update(i)
+            time.sleep(0.2)
 
 
-# if __name__ == "__main__":
-#     main()
+if __name__ == "__main__":
+    main()
 
 
 
